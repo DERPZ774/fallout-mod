392ef2f9fd0336f5aab028c6c001e8b3fd4e4078
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2023-06-25 02:07:17 -0400

    Fixed NukaColaMachine not rendering multiple bottles

--- src/main/java/com/derpz/nukaisles/client/renderer/NukaColaMachineBlockEntityRenderer.java
+++ src/main/java/com/derpz/nukaisles/client/renderer/NukaColaMachineBlockEntityRenderer.java
@@ -16,6 +16,8 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -37,41 +39,57 @@ public class NukaColaMachineBlockEntityRenderer extends GeoBlockRenderer<NukaCol
     public void actuallyRender(PoseStack pPoseStack, NukaColaMachineBlockEntity pBlockEntity, BakedGeoModel model, RenderType renderType, MultiBufferSource pBufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         super.actuallyRender(pPoseStack, pBlockEntity, model, renderType, pBufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-        ItemStack itemStack = pBlockEntity.getRenderStack();
-        if(itemStack.getItem() instanceof NukaColaItem) {
-            pPoseStack.pushPose();
-            //1st slot render
-            if (pBlockEntity.itemHandler.getStackInSlot(1).getItem() instanceof NukaColaItem) {
-                pPoseStack.translate(0.1f, 0.74f, 0.72f);
-                pPoseStack.scale(0.24f, 0.24f, 0.24f);
-                pPoseStack.mulPose(Axis.XP.rotationDegrees(90));
-                pPoseStack.mulPose(Axis.YP.rotationDegrees(90));
-                pPoseStack.mulPose(Axis.XN.rotationDegrees(-25));
-            } else if (pBlockEntity.itemHandler.getStackInSlot(2).getItem() instanceof NukaColaItem) {
-                pPoseStack.translate(0.13f, 0.74f, 0.75f);
-                pPoseStack.scale(0.24f, 0.24f, 0.24f);
-                pPoseStack.mulPose(Axis.XP.rotationDegrees(90));
-                pPoseStack.mulPose(Axis.YP.rotationDegrees(90));
-                pPoseStack.mulPose(Axis.XN.rotationDegrees(25));
-            }
+//        ItemStack itemStack = pBlockEntity.getRenderStack();
 
-            switch (pBlockEntity.getBlockState().getValue(NukaColaMachineBlock.FACING)) {
-                case NORTH -> pPoseStack.mulPose(Axis.ZP.rotationDegrees(0));
-                case EAST -> pPoseStack.mulPose(Axis.ZP.rotationDegrees(90));
-                case SOUTH -> pPoseStack.mulPose(Axis.ZP.rotationDegrees(180));
-                case WEST -> pPoseStack.mulPose(Axis.ZP.rotationDegrees(270));
-            }
 
-            itemRenderer.m_269128_(itemStack, ItemDisplayContext.GUI, getLightLevel(Objects.requireNonNull(pBlockEntity.getLevel()),
-                            pBlockEntity.getBlockPos()),
-                    OverlayTexture.NO_OVERLAY, pPoseStack, pBufferSource, pBlockEntity.getLevel(), pBlockEntity.getRenderStack().getCount());
-            pPoseStack.popPose();
+        for (int i = 1; i < pBlockEntity.itemHandler.getSlots(); i++) {
+            ItemStack stack = pBlockEntity.getRenderStack(i);
+            if (stack.getItem() instanceof NukaColaItem) {
+                pPoseStack.pushPose();
+                //1st slot render
+                Direction facing = pBlockEntity.getBlockState().getValue(NukaColaMachineBlock.FACING);
+                switch (i) {
+                    case 1 -> {
+                        if (facing == Direction.WEST) {
+                            pPoseStack.translate(0.18f, 0.74f, 0.71f);
+                            pPoseStack.mulPose(Axis.YN.rotationDegrees(25));
+                        } else if (facing == Direction.EAST) {
+                            pPoseStack.translate(0.82f, 0.74f, 0.29f);
+                            pPoseStack.mulPose(Axis.YN.rotationDegrees(25));
+                        } else if (facing == Direction.NORTH) {
+                            pPoseStack.translate(0.29f, 0.74f, 0.16f);
+                            pPoseStack.mulPose(Axis.YP.rotationDegrees(65));
+                        } else if (facing == Direction.SOUTH) {
+                            pPoseStack.translate(0.71f, 0.74f, 0.82f);
+                            pPoseStack.mulPose(Axis.YP.rotationDegrees(65));
+                        }
+                        pPoseStack.scale(0.24f, 0.24f, 0.24f);
+                    }
+                    case 2 -> {
+                        if (facing == Direction.WEST) {
+                            pPoseStack.translate(0.18f, 0.74f, 0.91f);
+                            pPoseStack.mulPose(Axis.YP.rotationDegrees(27));
+                        } else if (facing == Direction.EAST){
+                            pPoseStack.translate(0.18f, 0.74f, 0.91f);
+                            pPoseStack.mulPose(Axis.YP.rotationDegrees(27));
+                        }
+//                            pPoseStack.translate(0.13f, 0.74f, 0.75f);
+//                        }
+                            pPoseStack.scale(0.24f, 0.24f, 0.24f);
+
+                            // Add more cases as needed
+                        }
+                    }
+
+                    itemRenderer.m_269128_(stack, ItemDisplayContext.FIXED, getLightLevel(Objects.requireNonNull(pBlockEntity.getLevel()), pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY, pPoseStack, pBufferSource, pBlockEntity.getLevel(), pBlockEntity.getRenderStack(i).getCount());
+                    pPoseStack.popPose();
+                }
+                /// TODO: 5/25/2023 Rendering all bottles
+            }
         }
-        /// TODO: 5/25/2023 Rendering all bottles
+        private int getLightLevel(Level level, BlockPos pos) {
+            int bLight = level.getBrightness(LightLayer.BLOCK, pos);
+            int sLight = level.getBrightness(LightLayer.SKY, pos);
+            return LightTexture.pack(bLight, sLight);
+        }
     }
-    private int getLightLevel(Level level, BlockPos pos) {
-        int bLight = level.getBrightness(LightLayer.BLOCK, pos);
-        int sLight = level.getBrightness(LightLayer.SKY, pos);
-        return LightTexture.pack(bLight, sLight);
-    };
-}
